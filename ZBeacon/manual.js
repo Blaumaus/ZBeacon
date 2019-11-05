eb7480993d3acc41f99cf8eb26b6bc96a6456102
anon33rc4 <57300261+anon33rc4@users.noreply.github.com>
2019-11-05 16:31:55 -0500

    1.0.3

--- ZBeacon/manual.js
+++ ZBeacon/manual.js
@@ -1,5 +1,5 @@
 document.getElementById("newIdentity").onclick = function() {
-        browser.runtime.sendMessage({
+        alert("Are you sure? If you need to back out, exit this tab."); browser.runtime.sendMessage({
               data: "msg"
-        })
+        });
 }
